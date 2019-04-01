c257e9b6284ea1191d28d7c8150c19755a0ba3bc
kylebib <kylebib@users.noreply.github.com>
2019-04-01 13:37:47 -0700

    Update TrigProject.cpp

--- TrigProject.cpp
+++ TrigProject.cpp
@@ -1,16 +1,23 @@
 /****************************************************************************
- *                          Trigonometry 2019                               *
- *                   Main Group Project Spring Class                        *
+ *                   Group Trigonometry Project Spring 2019                 *
  *                                                                          *
  *      This program is an exploration of trigonometry from the prospect    *
  *      of a right angle triangle, law of Cosine and the usage of the       *
- *      distance formula.                                                   *
+ *      distance formula. Additionally, the program provides calculations   *
+ *      to other geometrical shapes: the circle and the trapezoid.          *
  *                                                                          *
- *      @Author:    Ines Campos			                                    *
- *      @Created:   March 25, 2019                                          *
- ***************************************************************************/
+ *                      Authors: Kyle Bibeault,                             *
+ *                               Ines Campos,                               *
+ *                               Luis Rodriguez,                            *
+ *                               Chris Flores,                              *
+ *                               Theo Brown                                 *
+ *                                                                          *
+ *                      Created on: March 30, 2019                          *
+ *****************************************************************************/
 
+#include <cstdlib>
 #include <iostream>
+#include <cmath>
 #include <iomanip>
 #include <regex>
 #include <limits>
@@ -20,279 +27,158 @@ using namespace std;
 ///Function Protypes Framework
 void intro();
 void mainMenu();
-void subMenu();
-int  getChoice();
-int  getSubChoice();
+int getChoice();
+int getTriangleChoice();
 void goodbyeMessage();
+void dummyMessage();
+
+int main() {
+
+    int choice,
+            triangleChoice;
+
+    do {
+        mainMenu();
+
+        choice = getChoice();
+
+        switch (choice) {
+
+            //Triangles
+            case 1:
+                do {
+                    triangleChoice = getTriangleChoice();
+
+                    switch (triangleChoice) {
+
+                        case 1:
+                            dummyMessage();
+                            break;
+                        case 2:
+                            dummyMessage();
+                            break;
+
+                        case 3:
+                            dummyMessage();
+                            break;
+
+                        default:
+                            break;
+                        }
+                    
+
+                } while (choice != 4);
+
+                break;
+                
+                //Circles
+            case 2:
+                dummyMessage();
+                break;
+
+                //Trapezoids
+            case 3:
+                dummyMessage();
+                break;
+
+            default:
+                break;
+        }
 
 
-void checkDouble(string &);
-void checkDoubleMinus(string &);
-void checkInt(string &);
-void checkChar(string &);
+    } while (choice != 4);
 
-int main()
-{
-	int choice, subChoice;
-	
+    goodbyeMessage();
 
-	do {
-		mainMenu();
-
-		choice = getChoice();
-
-		if (choice != 4) {
-			switch (choice) {
-
-			case 1:
-				do {
-					subMenu();
-
-					subChoice = getSubChoice();
-
-					if (choice != 4) {
-						switch (subChoice) {
-
-						case 1:
-							exploreSin();
-							break;
-						case 2:
-							exploreCos();
-							break;
-
-						case 3:
-							exploreTan();
-							break;
-
-						default:
-							break;
-						}
-					}
-
-				} while (choice != 4);
-				
-				cin.ignore(numeric_limits<int>::max(), '\n');
-
-				break;
-
-			case 2:
-				printLawOfCosine();
-				break;
-
-			case 3:
-				printDistFormulaLawCosine();
-				break;
-
-			default:
-				break;
-			}
-		}
-
-	} while (choice != 4);
-
-
-	goodbyeMessage();
-
-	return 0;
+    return 0;
 }
 
 void goodbyeMessage() {
 
-	cout << "\n\n\n";
-	cout << setw(10) << "" << "Thanks for using the Program, Goodbye\n\n";
-	cout << setw(10) << "" << "Press any key to continue . . .\n\n";
-	cin.ignore();
-	cin.get();
+    cout << "\n\n\n";
+    cout << setw(10) << "" << "Thanks for using the Program, Goodbye\n\n";
+    cout << setw(10) << "" << "Press any key to continue . . .\n\n";
+    cin.ignore();
+    cin.get();
+
+}
+
+void intro() {
+    cout << setw(15) << "" << "WELCOME TO TRIG 2019 Ver. 2.0.0\n";
+    cout << setw(10) << "" << "___________________________________________\n";
+
+    cout << "\n\n\n\n";
+
+    cout << setw(15) << "" << "       000000000000		000000000000	\n";
+    cout << setw(15) << "" << "	    0000000000000000	      0000000000000000	\n";
+    cout << setw(15) << "" << "	   000000000000000000	     000000000000000000	\n";
+    cout << setw(15) << "" << "	  00000000000000000000	    00000000000000000000\n";
+    cout << setw(15) << "" << "	 0000000000000000000000	   0000000000000000000000\n";
+    cout << setw(15) << "" << "	000000000000000000000000  000000000000000000000000\n";
+    cout << setw(15) << "" << "0000000000000000000000000000000000000000000000000000\n";
+    cout << setw(15) << "" << "000000000000000                       00000000000000\n";
+    cout << setw(15) << "" << "	00000000000000     Kyle Bibeault     0000000000000\n";
+    cout << setw(15) << "" << "	  000000000000     Theodis Brown     00000000000\n";
+    cout << setw(15) << "" << "	    0000000000     Ines Campos	     000000000	\n";
+    cout << setw(15) << "" << "	      00000000     Chris Flores      0000000	\n";
+    cout << setw(15) << "" << "	        000000     Luis Rodriguez    00000	\n";
+    cout << setw(15) << "" << "	          0000                       000	\n";
+    cout << setw(15) << "" << "	            00000000000000000000000000		\n";
+    cout << setw(15) << "" << "	              0000000000000000000000		\n";
+    cout << setw(15) << "" << "	                000000000000000000		\n";
+    cout << setw(15) << "" << "	                   0000000000000		\n";
+    cout << setw(15) << "" << "	                     000000000			\n";
+    cout << setw(15) << "" << "	                       00000			\n";
+    cout << setw(15) << "" << "	                         0	\n";
+    cout << setw(10) << "" << "\nPress any key to continue . . .\n";
 
 
-void intro(){
-	cout << setw(15) << "" << "WELCOME TO TRIG 2019 Ver. 2.0.0\n";
-	cout << setw(10) << "" << "___________________________________________\n";
 
-	cout << "\n\n\n\n";
-
-	cout << setw(15) << "" << "       000000000000		000000000000			\n";
-	cout << setw(15) << "" << "	    0000000000000000	      0000000000000000			\n";
-	cout << setw(15) << "" << "	   000000000000000000	     000000000000000000			\n";
-	cout << setw(15) << "" << "	  00000000000000000000	    00000000000000000000		\n";
-	cout << setw(15) << "" << "	 0000000000000000000000	   0000000000000000000000		\n";
-	cout << setw(15) << "" << "	000000000000000000000000  000000000000000000000000		\n";
-	cout << setw(15) << "" << "0000000000000000000000000000000000000000000000000000		\n";
-	cout << setw(15) << "" << "000000000000000                       00000000000000		\n";
-	cout << setw(15) << "" << "	00000000000000     Kyle Bibeault     0000000000000		\n";
-	cout << setw(15) << "" << "	  000000000000     Theodis Brown     00000000000		\n";
-	cout << setw(15) << "" << "	    0000000000     Ines Campos	     000000000			\n";
-	cout << setw(15) << "" << "	      00000000     Chris Flores      0000000			\n";
-	cout << setw(15) << "" << "	        000000     Luis Rodriguez    00000				\n";
-	cout << setw(15) << "" << "	          0000                       000				\n";
-	cout << setw(15) << "" << "	            00000000000000000000000000					\n";
-	cout << setw(15) << "" << "	              0000000000000000000000					\n";
-	cout << setw(15) << "" << "	                000000000000000000						\n";
-	cout << setw(15) << "" << "	                   0000000000000						\n";
-	cout << setw(15) << "" << "	                     000000000							\n";
-	cout << setw(15) << "" << "	                       00000							\n";
-	cout << setw(15) << "" << "	                         0								\n";
-	cout << setw(10) << "" << "\nPress any ket to continue . . .\n";
 
 
 }
 
+void mainMenu() {
 
-void mainMenu(){
+    system("cls");
 
-	system("cls");
+    cout << "\n\n\n";
+    cout << setw(10) << "" << "	 TRIGONOMETRY VERSION 2.0.0\n";
+    cout << setw(10) << "" << "___________________________________________\n";
 
-	cout << "\n\n\n";
-	cout << setw(10) << "" << "	 TRIGONOMETRY VERSION 2.0.0\n";
-	cout << setw(10) << "" << "___________________________________________\n";
+    cout << setw(10) << "" << "1. Triangles\n";
+    cout << setw(10) << "" << "2. Circles\n";
+    cout << setw(10) << "" << "3. Trapezoids\n";
+    cout << setw(10) << "" << "4. Exit program\n\n";
 
-	cout << setw(10) << "" << "1. Right Angle Trigonometry\n";
-	cout << setw(10) << "" << "2. Law of Cosine\n";
-	cout << setw(10) << "" << "3. Distance Formula and Law of Cosine\n";
-	cout << setw(10) << "" << "4. Exit program\n\n";
-
-	cout << setw(10) << "" << "Enter your Choice: ";
-	}
-
-
-void subMenu() {
-
-	system("cls");
-
-	cout << "\n\n\n";
-	cout << setw(10) << "" << " Right Angle Calculations\n";
-	cout << setw(10) << "" << "____________________________________________\n";
-	cout << setw(10) << "" << "If we know the size of angle A in a right"
-							  "angle triangle and one of the three other "
-						   	  "sides, then we can find all the values of "
-							  "the triangle."
-							  "Likewise, if we know two sides of a right "
-							  "angle triangle, we can find all the other"
-							  "values of the triangle\n";
-	cout << setw(10) << "" << "____________________________________________\n";
-
-	cout << setw(20) << "" << "To Explore sin: Press 1\n";
-	cout << setw(20) << "" << "To Explore cos: Press 2\n";
-	cout << setw(20) << "" << "To Explore tan: Press 3\n";
-	cout << setw(20) << "" << "To Exit		 : Press 4\n\n";
-
-	cout << setw(10) << "" << "Enter your Choice: ";
+    cout << setw(10) << "" << "Enter your Choice: ";
 }
-
 
 int getChoice() {
 
-	string strChoice;
-	cin >> strChoice;
-	checkInt(strChoice);
-
-	return stoi(strChoice);
-	}
-
-int getSubChoice() {
-
-	string strSubChoice;
-	cin >> strSubChoice;
-	checkInt(strSubChoice);
-
-	return stoi(strSubChoice);
-
+    int choice;
+    cin >> choice;
+    while (choice < 1 || choice > 4) {
+        cout << "The only valid Choices are 1-4. Please re-enter: ";
+        cin >> choice;
+    }
+    return choice;
 }
 
-/****************************************************************************
- *                    FUNCTION DESCPRIPTION checkDouble()                   *
- *      This function takes a floating point number as a string and use the *
- *      string to check if the entered floating point matches the language  *
- *      defined for a positive floating point number. If not, ask the user  *
- *      to re-enter a valid input.                                          *
- *                                                                          *
- *      @Precondition:  This function takes a string argument as reference  *
- *                      to be matched with a defined language for a positive*
- *                      floating point number.                              *
- *      @Postcondition: This function does not return anything.             *
- ***************************************************************************/
-void checkDouble(string &s) {
-	regex myDouble("^(0|([1-9][0-9]*))(\\.[0-9]+)?$");
-	while (!(regex_match(s, myDouble))) {
-		cout << setw(10) << "" << "Wrong Input. Enter a Positive"
-			"Decimal Number. Re-Enter: ";
-		cin >> s;
-		if (regex_match(s, myDouble)) {
-			break;
-		}
-	}
+void dummyMessage() {
+
+    cout << setw(10) << "" << "This page has not yet been built\n";
+    system("pause");
 }
 
-/****************************************************************************
- *                  FUNCTION DESCRIPTION checkDoubleMinus()                 *
- *                                                                          *
- *      This function takes a floating point number as a string and use the *
- *      string to check if the entered floating point matches the language  *
- *      defined for a any floating point number. If not, ask the user to    *
- *      re-enter a valid input.                                             *
- *                                                                          *
- *      @Precondition:  This function takes a string argument as reference  *
- *                      to be matched with a defined language for any       *
- *                      floating point numbers.                             *
- *      @Postcondition: This function does not return anything.             *
- ***************************************************************************/
-void checkDoubleMinus(string &s) {
-	regex myDouble("^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$");
-	while (!(regex_match(s, myDouble))) {
-		cout << setw(10) << "" << "Wrong Input. Enter a Decimal Number for "
-			"the coordinate. Re-Enter: ";
-		cin >> s;
-		if (regex_match(s, myDouble)) {
-			break;
-		}
-	}
-}
+int getTriangleChoice() {
 
-/****************************************************************************
- *                    FUNCTION DESCRIPTION checkChar()                      *
- *                                                                          *
- *      This function takes a character as a string and use the string to   *
- *      check if the entered character matches the language defined for the *
- *      characters defined in the function. If not, ask the user to re-enter*
- *      a valid input.                                                      *
- *                                                                          *
- *      @Precondition:  This function takes a string argument as reference  *
- *                      to be matched with a defined language for a positive*
- *                      floating point number                               *
- *      @Postcondition: This function does not return anything.             *
- ***************************************************************************/
-void checkChar(string &s) {
-	regex myChar("^[a-c]?$");
-	while (!(regex_match(s, myChar))) {
-		cout << setw(10) << "" << "Wrong Input. Enter a/b,c only. please"
-			"Re-Enter: ";
-		cin >> s;
-		if (regex_match(s, myChar)) {
-			break;
-		}
-	}
-}
+    int triChoice;
+    cin >> triChoice;
+    while (triChoice < 1 || triChoice > 4) {
+        cout << "The only valid Choices are 1-3. Please re-enter: ";
+        cin >> triChoice;
+    }
+    return triChoice;
 
-/****************************************************************************
- *                    FUNCTION DESCRIPTION checkInt()                       *
- *                                                                          *
- *      This function takes an integer number as a string and use the       *
- *      string to check if the entered integer matches the language defined *
- *      for an integer. If not, ask the user to re-enter a valid input      *
- *                                                                          *
- *      @Precondition:  This function takes a string argument as reference  *
- *                      to be matched with a defined language for a positive*
- *                      floating point number                               *
- *      @Postcondition: This function does not return anything.             *
- ***************************************************************************/
-void checkInt(string &s) {
-	regex myChar("^[1-4]?$");
-	while (!(regex_match(s, myChar))) {
-		cout << setw(10) << "" << "Wrong Input. Enter 1 - 4 only. please"
-			"Re-Enter: ";
-		cin >> s;
-		if (regex_match(s, myChar)) {
-			break;
-		}
-	}
 }
