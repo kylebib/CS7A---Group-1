8643fbf17a6d2a7c663b882fed5e71e6e2825d99
kylebib <kylebib@users.noreply.github.com>
2019-04-01 20:08:14 -0700

    Update TrigProject.cpp

--- TrigProject.cpp
+++ TrigProject.cpp
@@ -13,14 +13,12 @@
  *                               Theo Brown                                 *
  *                                                                          *
  *                      Created on: March 30, 2019                          *
- *****************************************************************************/
+ ****************************************************************************/
 
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-#include <regex>
-#include <limits>
 
 using namespace std;
 
@@ -34,10 +32,26 @@ void dummyMessage();
 void pause();
 void clearScreen();
 
+
+void   trapezoidMenu();
+int    trapezoidChoice();
+double trapezoidCalcArea(double, double, double);
+double trapezoidCalcPerimeter(double, double, double, double);
+double trapezoidCalcHeight(double, double, double);
+void   trapezoidDisplayOutput(int);
+
+
+double triangleCalcDistance();
+double angToRad(double);
+double distanceFormula(double, double, double, double);
+
+const double PI = 3.14159;
+
 int main() {
 
     int choice,
-        triangleChoice;
+        triangleChoice,
+        trapChoice;
 
     intro();
     
@@ -62,11 +76,9 @@ int main() {
                         case 2:
                             dummyMessage();
                             break;
-
                         case 3:
                             dummyMessage();
                             break;
-
                         default:
                             break;
                         }
@@ -82,7 +94,28 @@ int main() {
 
                 //Trapezoids
             case 3:
-                dummyMessage();
+                do {
+                trapezoidMenu();
+                
+                trapChoice = trapezoidChoice();
+
+                    switch (trapChoice) {
+
+                        case 1:
+                            trapezoidDisplayOutput(trapChoice);
+                            break;
+                        case 2:
+                            trapezoidDisplayOutput(trapChoice);
+                            break;
+                        case 3:
+                            trapezoidDisplayOutput(trapChoice);
+                            break;
+                        default:
+                            break;
+                        }
+                    
+                } while (trapChoice != 4);
+                
                 break;
 
             default:
@@ -189,4 +222,189 @@ void pause() {
 
 void clearScreen() {
     system("cls");
+}
+
+void trapezoidMenu() {
+    
+    clearScreen();
+
+    cout << "\n\n\n";
+    cout << setw(10) << "" << "	 TRAPEZOIDS\n";
+    cout << setw(10) << "" << "________________\n\n";
+
+    cout << setw(10) << "" << "1. Calculate Area\n";
+    cout << setw(10) << "" << "2. Calculate Height\n";
+    cout << setw(10) << "" << "3. Calculate Distance Formula\n";
+    cout << setw(10) << "" << "4. Go Back\n\n";
+    
+    cout << setw(10) << "" << "Enter your Choice: ";
+    
+}
+
+int trapezoidChoice() {
+    
+    int trapChoice;
+    cin >> trapChoice;
+    while (trapChoice < 1 || trapChoice > 4) {
+        cout << "The only valid Choices are 1-4. Please re-enter: ";
+        cin >> trapChoice;
+    }
+    return trapChoice;
+    
+}
+
+double trapezoidCalcArea(double height, double sideA, double sideB) {
+    
+    return ((sideA + sideB) / 2) * height;
+  
+}
+
+double trapezoidCalcPerimeter(double baseC, double baseD, double sideA, double sideB) {
+    
+    return baseC + baseD + sideA + sideB;
+  
+}
+
+double trapezoidCalcHeight(double area, double sideA, double sideB) {
+    
+    return 2 * (area / (sideA + sideB));
+    
+}
+
+void trapezoidDisplayOutput(int trapChoice) {
+    
+    clearScreen();
+    
+    int trapChoice1 = trapChoice;
+    int height;
+    int sideA;
+    int sideB;
+    int area;
+    
+    cout << "\n\n\n";
+    
+    
+    if (trapChoice1 == 1) {
+        
+    cout << setw(15) << "" << "The Area of a Trapezoid\n";
+    cout << setw(10) << "" << "______________________________\n\n";
+    cout << setw(10) << "" << "In order to calculate the area of a trapezoid,\n";
+    cout << setw(10) << "" << "please enter the following variables.\n\n";
+    cout << setw(10) << "" << "Enter height of trapezoid: ";
+    cin >> height;
+    
+    while (height < 0) {
+        cout << setw(10) << "" << "Please enter positive height: ";
+        cin >> height;
+    }
+    
+    cout << setw(10) << "" << "Enter left side of trapezoid: ";
+    cin >> sideA;
+    
+    while (sideA < 0) {
+        cout << setw(10) << "" << "Please enter positive number: ";
+        cin >> sideA;
+    }
+    
+    cout << setw(10) << "" << "Enter left side of trapezoid: ";
+    cin >> sideB;
+    
+    while (sideB < 0) {
+        cout << setw(10) << "" << "Please enter positive number: ";
+        cin >> sideB;
+    }
+    
+    area = trapezoidCalcArea(height, sideA, sideB);
+    
+    cout << "\n\n";
+    
+    cout << setw(10) << "" << "The area is: " << area << " units squared.";
+    
+    
+    }
+    
+    
+    if (trapChoice1 == 2) {
+        
+    cout << setw(15) << "" << "The Perimeter of a Trapezoid";
+    
+    }
+    
+    if (trapChoice1 == 3) {
+        
+    cout << setw(15) << "" << "The Angles of a Trapezoid";
+    
+    }
+    
+    pause();
+    
+}
+
+
+//CHRIS FLORES
+/********************************************************************************
+*							distanceFormula()									*
+*			This function calculates the distance between a set of ordered		*
+*			pairs.																*
+*																				*
+*			Precondition: The user inputs 4 values which will be used as		*
+*			arguments.															*
+*																				*
+*			Postcondition: This function returns the distance by using			*
+*			distance formula													*
+*																				*
+********************************************************************************/
+double distanceFormula(double x1, double x2, double y1, double y2) {
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
+
+/********************************************************************************
+*								  angToRad()
+*				This function converts angles to radians by multiplying theta
+*				by PI over 180.
+*
+*				Precondition:
+*
+*				Postcondition:
+*
+*
+*********************************************************************************/
+double angToRad(double angle) {
+	return (angle * PI) / 180;	// Pending variable name for angle 
+}
+
+/********************************************************************************
+*							triangleCalcDistance()
+*				This function
+*
+*
+*				Precondition:
+*				
+*				Postcondition:
+*
+********************************************************************************/
+double triangleCalcDistance() {
+
+	double x1, y1, x2, y2, x3, y3;
+
+	system("clear");
+	cout << "\n\n\n";
+	cout << setw(20) << "" << "Calculating Triangle Distance" << endl;
+	cout << setw(10) << "" << "________________________________________________" << endl << endl;
+	cout << setw(10) << "" << "Coordinates for Side a" << endl;
+	cout << setw(10) << "" << "Enter Point 1: ";
+	cin >> x1;
+	cout << setw(10) << "" << "Enter Point 2: ";
+	cin >> y1;
+	cout << "\n" << setw(10) << "" << "Coordinates for Side b" << endl;
+	cout << setw(10) << "" << "Enter Point 1: ";
+	cin >> x2;
+	cout << setw(10) << "" << "Enter Point 2: ";
+	cin >> y2;
+	cout << "\n" << setw(10) << "" << "Coordinates for Side c" << endl;
+	cout << setw(10) << "" << "Enter Point 1: ";
+	cin >> x3;
+	cout << setw(10) << "" << "Enter Point 2: ";
+	cin >> y3;
+
 }
